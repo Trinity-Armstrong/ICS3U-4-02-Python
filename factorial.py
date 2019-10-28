85e55473258bf908871f5d42e69487802592b8ff
Trinity-Armstrong <Trinity.Armstrong@mths.ca>
2019-10-28 03:16:30 +0000

    initial commit

---
+++ factorial.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Trinity Armstrong
+# Created on: October 2019
+# This program outputs the factorial of a positive integer
+
+
+def main():
+    # This function outputs the factorial of a positive integer
+
+    # Variables
+    factorial = 1
+    counter = 0
+
+    # Input
+    integer = int(input("Enter a positive integer: "))
+    print("")
+
+    # Process
+    while integer > counter:
+        counter += 1
+        factorial = factorial*counter
+
+    # Output
+    print("The factorial is ", factorial)
+
+
+if __name__ == "__main__":
+    main()
